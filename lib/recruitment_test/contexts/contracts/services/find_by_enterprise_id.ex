1f24e3e8e7e467010bbbe254f564290647e1bcb8
Victor Palha <jv.palha@gmail.com>
2026-02-12 20:00:03 -0300

    fix: spec type returning on find enterprise

--- lib/recruitment_test/contexts/contracts/services/find_by_enterprise_id.ex
+++ lib/recruitment_test/contexts/contracts/services/find_by_enterprise_id.ex
@@ -8,7 +8,7 @@ defmodule RecruitmentTest.Contexts.Contracts.Services.FindByEnterpriseId do
   alias RecruitmentTest.Repo
   import Ecto.Query
 
-  @spec call(enterprise_id :: String.t()) :: {:ok, list()} | {:error, String.t()}
+  @spec call(enterprise_id :: String.t()) :: list() | {:error, String.t()}
   def call(enterprise_id) when is_uuid(enterprise_id) do
     from(c in Contract, where: c.enterprise_id == ^enterprise_id)
     |> Repo.all()
