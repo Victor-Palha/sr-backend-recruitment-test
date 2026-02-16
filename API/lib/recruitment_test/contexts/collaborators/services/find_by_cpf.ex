39d2193df29f29d7f9485cef2e317c46104e668d
Victor Palha <jv.palha@gmail.com>
2026-02-16 12:31:07 -0300

    fix: cpf validation

--- API/lib/recruitment_test/contexts/collaborators/services/find_by_cpf.ex
+++ API/lib/recruitment_test/contexts/collaborators/services/find_by_cpf.ex
@@ -8,6 +8,8 @@ defmodule RecruitmentTest.Contexts.Collaborators.Services.FindByCpf do
   def call(cpf) when is_binary(cpf) and byte_size(cpf) > 0 do
     Logger.debug("Finding collaborator by CPF", service: "collaborators.find_by_cpf")
 
+    cpf = sanitize_cpf(cpf)
+
     case Repo.get_by(Collaborator, cpf: cpf) do
       nil -> {:error, :not_found}
       collaborator -> {:ok, collaborator}
@@ -15,4 +17,9 @@ defmodule RecruitmentTest.Contexts.Collaborators.Services.FindByCpf do
   end
 
   def call(_cpf), do: {:error, :not_found}
+
+  defp sanitize_cpf(cpf) do
+    cpf
+    |> String.replace(~r/[^0-9]/, "")
+  end
 end
